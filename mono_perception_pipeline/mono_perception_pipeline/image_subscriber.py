2b535954628adc1dec8b81e17b59a7ecc85bd741
Tarun Shivakumar <tarunshivakumar1@gmail.com>
2026-09-16 01:50:23 -0400

    tidied some shit up in the sub node

--- mono_perception_pipeline/mono_perception_pipeline/image_subscriber.py
+++ mono_perception_pipeline/mono_perception_pipeline/image_subscriber.py
@@ -25,7 +25,7 @@ class ImageSubscriber(Node):
 
         self.bridge = CvBridge()
 
-        self.tf_broadcaster = TransformBroadcaster()
+        self.tf_broadcaster = TransformBroadcaster(self)
 
         self.path_publisher = self.create_publisher(Path, '/camera/path', 10)
         self.pose_publisher = self.create_publisher(PoseStamped, '/camera/pose', 10)
@@ -43,67 +43,65 @@ class ImageSubscriber(Node):
         path = Path()
         path.header = msg.header()
 
-        pose_stamped = PoseStamped()
-        pose_stamped.header = msg.header()
-
-        pose = Pose()
-
-        point = Point()
-        quat = Quaternion()
-
         R1, t1, good_old, good_new = compute_optical_flow(image_a, image_b)
         R2, t2, good_old, good_new = compute_optical_flow(image_b, image_c)
 
-        # Pair 1
+        # Pair 1 (image a, b) - pose and path
         self.t_global = np.add(self.t_global, np.matmul(self.R_global, t1))
         self.R_global = np.matmul(R1, self.R_global)
 
         scipy_rotation = Rotation.from_matrix(self.R_global)
         global_quat_matrix = scipy_rotation.as_quat()
         global_translation_matrix = self.t_global.flatten()
-        
-        point.x = global_translation_matrix[0]
-        point.y = global_translation_matrix[1]
-        point.z = global_translation_matrix[2]
-        
-        quat.x = global_quat_matrix[0]
-        quat.y = global_quat_matrix[1]
-        quat.z = global_quat_matrix[2]
-        quat.w = global_quat_matrix[3]
 
-        pose.position = point
-        pose.orientation = quat
+        pose_ab = Pose()
+        pose_ab.position = Point(x=global_translation_matrix[0], y=global_translation_matrix[1], z=global_translation_matrix[2])
+        pose_ab.orientation = Quaternion(x=global_quat_matrix[0], y=global_quat_matrix[1], z=global_quat_matrix[2], w=global_quat_matrix[3])
 
-        pose_stamped = pose
+        pose_stamped_ab = PoseStamped()
+        pose_stamped_ab.header = msg.header()
+        pose_stamped_ab.pose = pose_ab
 
-        path.poses.append(pose_stamped)
+        path.poses.append(pose_stamped_ab)
 
-        self.pose_publisher.publish(pose)
+        self.pose_publisher.publish(pose_stamped_ab)
         self.path_publisher.publish(path)
 
-        #Pair 2
+        # Pair 1 (image a, b) - tf broadcast
+        transform_stamped_ab = TransformStamped()
+        transform_stamped_ab.header.stamp = self.get_clock().now().to_msg()
+        transform_stamped_ab.header.frame_id = "world"
+        transform_stamped_ab.child_frame_id = "camera"
+
+        transform_ab = Transform()
+        transform_ab.translation = Vector3(x=global_translation_matrix[0], y=global_translation_matrix[1], z=global_translation_matrix[2])
+        transform_ab.rotation = Quaternion(x=global_quat_matrix[0], y=global_quat_matrix[1], z=global_quat_matrix[2], w=global_quat_matrix[3])
+        transform_stamped_ab.transform = transform_ab
+
+        self.tf_broadcaster.sendTransform(transform_stamped_ab)
+
+        # Pair 2 (image b, c) - pose and path
         self.t_global = np.add(self.t_global, np.matmul(self.R_global, t2))
         self.R_global = np.matmul(R2, self.R_global)
-                
-        point.x = self.t_global[0]
-        point.y = self.t_global[1]
-        point.z = self.t_global[2]
-                
-        quat.x = self.R_global[0]
-        quat.y = self.R_global[1]
-        quat.z = self.R_global[2]
-        quat.w = self.R_global[3]
-        
-        pose.position = point
-        pose.orientation = quat
-        
-        pose_stamped = pose
 
-        path.poses.append(pose_stamped)
+        scipy_rotation = Rotation.from_matrix(self.R_global)
+        global_quat_matrix = scipy_rotation.as_quat()
+        global_translation_matrix = self.t_global.flatten()
 
-        self.pose_publisher.publish(pose)
+        pose_bc = Pose()
+        pose_bc.position = Point(x=global_translation_matrix[0], y=global_translation_matrix[1], z=global_translation_matrix[2])
+        pose_bc.orientation = Quaternion(x=global_quat_matrix[0], y=global_quat_matrix[1], z=global_quat_matrix[2], w=global_quat_matrix[3])
+
+        pose_stamped_bc = PoseStamped()
+        pose_stamped_bc.header = msg.header()
+        pose_stamped_bc.pose = pose_bc
+
+        path.poses.append(pose_stamped_bc)
+
+        self.pose_publisher.publish(pose_stamped_bc)
         self.path_publisher.publish(path)
 
+        # Pair 1 (image a, b) - point cloud
         frame_a_keypoints, frame_b_keypoints = compute_keypoints(image_a, image_b)
         point_cloud_matrix = compute_point_cloud(R1, t1, R2, t2, self.mtx, frame_a_keypoints, frame_b_keypoints)
         point_cloud_matrix = np.array(point_cloud_matrix, dtype=np.float32)
@@ -125,25 +123,36 @@ class ImageSubscriber(Node):
 
         self.point_cloud_publisher.publish(point_cloud)
 
-        transform_stamped = TransformStamped()
-        transform_stamped.header.stamp = self.get_clock().now().to_msg()
-        transform_stamped.header.frame_id = "world"
-        transform_stamped.child_frame_id = "camera"
+        # Pair 2 (image b, c) - point cloud
+        frame_b_keypoints_bc, frame_c_keypoints = compute_keypoints(image_b, image_c)
+        point_cloud_matrix_bc = compute_point_cloud(R1, t1, R2, t2, self.mtx, frame_b_keypoints_bc, frame_c_keypoints)
+        point_cloud_matrix_bc = np.array(point_cloud_matrix_bc, dtype=np.float32)
 
-        transform = Transform()
+        point_cloud_bc = PointCloud2()
+        point_cloud_bc.header = msg.header()
+        point_cloud_bc.height = 1
+        point_cloud_bc.width = point_cloud_matrix_bc.shape[0]
+        point_cloud_bc.fields = point_field
+        point_cloud_bc.is_bigendian = False
+        point_cloud_bc.point_step = 12
+        point_cloud_bc.row_step = 12 * point_cloud_matrix_bc.shape[0]
+        point_cloud_bc.data = point_cloud_matrix_bc.tobytes()
+        point_cloud_bc.is_dense = True
 
-        vector3 = Vector3()
-        vector3.x = global_translation_matrix[0]
-        vector3.y = global_translation_matrix[1]
-        vector3.z = global_translation_matrix[2]
+        self.point_cloud_publisher.publish(point_cloud_bc)
 
-        transform.translation = vector3
-        transform.rotation = quat
+        # Pair 2 (image b, c) - tf broadcast
+        transform_stamped_bc = TransformStamped()
+        transform_stamped_bc.header.stamp = self.get_clock().now().to_msg()
+        transform_stamped_bc.header.frame_id = "world"
+        transform_stamped_bc.child_frame_id = "camera"
 
-        self.tf_broadcaster.sendTransform(TransformStamped)
+        transform_bc = Transform()
+        transform_bc.translation = Vector3(x=global_translation_matrix[0], y=global_translation_matrix[1], z=global_translation_matrix[2])
+        transform_bc.rotation = Quaternion(x=global_quat_matrix[0], y=global_quat_matrix[1], z=global_quat_matrix[2], w=global_quat_matrix[3])
+        transform_stamped_bc.transform = transform_bc
 
-
-
+        self.tf_broadcaster.sendTransform(transform_stamped_bc)
 
 
 def main(args=None):
